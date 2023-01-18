b02f98e9a38737271b44c5afe35125253301da9f
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-01-18 03:08:52 +0100

    tested API

--- controllers/Request.js
+++ controllers/Request.js
@@ -7,7 +7,19 @@ const request = async (req, res) => {
         max_tokens: 7,
         temperature: 0,
     });
-    console.log(response.data.choices[0].text)
+    res.send(response.data.choices[0].text)
 }
+
+// const requestAPI = async (req, res) => {
+//     const response = await openai.createCompletion({
+//         model: "text-davinci-003",
+//         prompt: "Say this is a test",
+//         max_tokens: 7,
+//         temperature: 0,
+//     });
+//     console.log(response.data.choices[0].text)
+// }
+
+// requestAPI()
 
 module.exports = request
