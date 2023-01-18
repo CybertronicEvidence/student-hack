d13d6f990bcd771a74ffcc989e82ab417fd8666d
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-01-18 03:12:28 +0100

    calling request route

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const request = require('./routes/request')
+const dotenv = require('dotenv')
+dotenv.config()
 
 
 const app = express()
 const port = 5000
+
+app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/api/chat', request)
 
