1cb32918518521d85be96ae5a4e7eaa66a3b3a48
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-01-18 02:23:23 +0100

    called in route on the server

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const request = require('./routes/request')
 
 
 const app = express()
 const port = 5000
+
+app.use('/api/chat', request)
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
